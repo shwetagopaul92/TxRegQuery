87fd3be24a316356e1cf33e69f3f324a5ce15730
shwetagopaul <mail@shwetagopaul92.com>
2017-08-01 13:07:24 -0400

    adding getBigqueryRangeEqtl.R

---
+++ R/getBigqueryRangeEqtl.R
@@ -0,0 +1,36 @@
+#'query eQTL data from bigquery
+#'@import GenomicRanges
+#'@import bigrquery
+#'@import dplyr
+#'@import DBI
+#'@import magrittr
+#'@param mychr chromosome
+#'@param mystart start position
+#'@param myend end position
+#'@param myproject project name in bigquery
+#'@param mydataset database name in bigquery
+#'@param mybilling billing group in bigquery
+#'@examples
+#'\dontrun{
+#' mychrGo="17"
+#' mystartGo=41196312-500*1000
+#' myendGo=41322262+500*1000
+#' myprojectGo="cgc-xx-xxxx"
+#' mydatasetGo="txregnet"
+#' mybillingGo = "cgc-xx-xxxx"
+#' res_eqtl=getBigqueryRangeEqtl(mychrGo,mystartGo,myendGo,myprojectGo,mydatasetGo,mybillingGo)
+#' res_eqtl
+#' }
+#' @export
+getBigqueryRangeEqtl <- function(mychr, mystart, myend, myproject, mydataset, mybilling){
+  require(bigrquery)
+  require(dplyr)
+  require(magrittr)
+  require(DBI)
+  require(GenomicRanges)
+  txreg_bq = DBI::dbConnect(dbi_driver(), project=myproject, dataset=mydataset, billing=mybilling) #make connection to bigquery
+  eqtl_tbl = txreg_bq %>% tbl("eQTL")  #make connection to required table
+  res = eqtl_tbl %>% filter(chr==mychr) %>% filter(snp_pos >= mystart) %>% filter(snp_pos <= myend) %>% as.data.frame
+  myrange=GRanges(res$chr, IRanges(res$snp_pos, width = 1), mcols=res)
+  myrange
+}
