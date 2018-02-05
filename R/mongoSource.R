665ee506a75b67a8e5548c553d6c8723a99a9206
shwetagopaul <mail@shwetagopaul92.com>
2018-02-05 13:38:01 -0500

    modifying mongoSource.R

--- R/mongoSource.R
+++ R/mongoSource.R
@@ -1,10 +1,12 @@
 #' connect to mongo database and retrieve collection names
 #' @import RMongo
 #' @import rJava
-#' @param db default database is txregnet
+#' @param dbName character with database name  default database is txregnet
+#' @param host character variable with url
+#' @param port port number
 #' @export
-mongoSource <- function(db="txregnet"){
-	con <- mongoDbConnect(db)
+mongoSource <- function(dbName,host,port){
+	con <- mongoDbConnect(dbName=db,host=url,port=port)
         cat("Connection established to",db,"database successfully! \n")
         cat("The collections present on the",db,"are: \n")
         collList = dbShowCollections(con)
