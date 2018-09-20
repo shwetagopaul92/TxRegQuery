e4e10e8d8ada668ac52c74f75817cfc7e6d37905
shwetagopaul <mail@shwetagopaul92.com>
2018-09-20 11:29:53 -0400

    Adding functionality to access Atlas data, version bump 0.6

---
+++ R/getAtlasRangeEqtl.R
@@ -0,0 +1,35 @@
+#'query eQTL from mongodb atlas
+#'@import GenomicRanges
+#'@import mongolite
+#'@param mychr chromosome
+#'@param mystart start position
+#'@param myend end position
+#'@param mycoll collection name in mongodb
+#'@param mydb database name in mongodb
+#'@param url character string with connection url to MongoDB Atlas cluster
+#'@examples
+#'\dontrun{
+#' require(GenomicRanges)
+#' require(mongolite)
+#' mychrGo=17
+#' mystartGo=41196312-500*1000
+#' myendGo=41322262+500*1000
+#' mycollGo = "Whole_Blood_allpairs_v7_eQTL"
+#' mydbGo="txregnet"
+#' url="mongodb+srv://test:test123!@cluster1-ag7nd.mongodb.net/test"
+#' res_eqtl=getMongoRangeEqtl(mychrGo,mystartGo,myendGo,mycollGo,mydbGo)
+#' res_eqtl
+#' }
+#' @export
+getMongoRangeEqtl<-function(mychr,mystart,myend,mycoll,mydb,url){
+  require(mongolite)
+  require(GenomicRanges)
+  my_collection = mongo(collection = mycoll, db = mydb, url=url) # connect
+  myquery=paste0('{',
+                 '"chr":',mychr,',',
+                 '"snp_pos" : {"$gte":',mystart,'},',
+                 '"snp_pos" : {"$lte":',myend,'}}')
+  res=my_collection$find(myquery)
+  myrange=GRanges(res$chr, IRanges(res$snp_pos, width = 1), mcols=res)
+  myrange
+}
---
+++ R/getAtlasRangeFp.R
@@ -0,0 +1,34 @@
+#'query Footprint data from mongodb atlas
+#'@import GenomicRanges
+#'@import mongolite
+#'@param mychr chromosome
+#'@param mystart start position
+#'@param myend end position
+#'@param mycoll collection name in mongodb
+#'@param mydb database name in mongodb
+#'@param url character string with connection url to MongoDB Atlas cluster
+#'@examples
+#'\dontrun{
+#' mychrGo="chr17"
+#' mystartGo=41196312-500*1000
+#' myendGo=41322262+500*1000
+#' mycollGo="fSkin_fibro_bicep_R_DS19745_hg19_FP"
+#' mydbGo="txregnet"
+#' url="mongodb+srv://test:test123!@cluster1-ag7nd.mongodb.net/test"
+#' res_fp=getMongoRangeFp(mychrGo,mystartGo,myendGo,mycollGo,mydbGo)
+#' res_fp
+#' }
+#' @export
+getAtlasRangeFp<-function(mychr,mystart,myend,url,mycoll,mydb){
+  require(mongolite)
+  require(GenomicRanges)
+  my_collection = mongo(collection = mycoll, db = mydb,url=url) # connect
+  myquery=paste0('{',
+                 '"chr":"',mychr,'",',
+                 '"start" : {"$gte":',mystart,'},',
+                 '"end" : {"$lte":',myend,'}}')
+  res=my_collection$find(myquery)
+  myrange=GRanges(res$chr, IRanges(res$start, res$end), mcols=res)
+  myrange
+
+}
